f4abba78451708a5e8d4d7f960ffc642e6767e7b
RhamzThev <rhamzthev@gmail.com>
2022-06-22 17:01:24 -0400

    finished tests. version one now in the finalizing stages

--- model/test/test_web_scraper.py
+++ model/test/test_web_scraper.py
@@ -1,4 +1,5 @@
 from model.web_scraper import WebScraper
+import os
 
 EXAMPLE_STATIC = "https://realpython.github.io/fake-jobs/jobs/senior-python-developer-0.html"
 
@@ -6,15 +7,67 @@ EXAMPLE_ONE = "https://www.demoblaze.com/"
 EXAMPLE_TWO = "https://demo.opencart.com/"
 EXAMPLE_THREE = "https://www.phptravels.net/"
 
-INPUT_FILE = "./input.txt"
-OUTPUT_FILE = "./output.txt"
+INPUT_FILE = "C:/Users/coolr/OneDrive/GitHub/WebScraper/model/test/txt/input.txt"
+OUTPUT_FILE = "C:/Users/coolr/OneDrive/GitHub/WebScraper/model/test/txt/output.txt"
 
-def test_find_keyword_static():
+TXT_FOLDER = "C:/Users/coolr/OneDrive/GitHub/WebScraper/model/test/txt/"
+
+def test_find_keyword_true():
     webScraper = WebScraper()
-    assert WebScraper._find_keyword(webScraper, EXAMPLE_THREE, "Library")
+    assert WebScraper._find_keyword(webScraper, EXAMPLE_STATIC, "Python")
+
+def test_find_keyword_false():
+    webScraper = WebScraper()
+    assert not WebScraper._find_keyword(webScraper, EXAMPLE_STATIC, "False Keyword")
+
+def test_find_dirs_zero():
+    webScraper = WebScraper()
+    length = len(WebScraper._find_dirs(webScraper, EXAMPLE_STATIC))
+    assert length == 1
+
+def test_find_dirs_non_zero():
+    webScraper = WebScraper()
+    length = len(WebScraper._find_dirs(webScraper, EXAMPLE_ONE))
+    assert length > 1
 
 def test_add_to_output():
-    assert False
+    webScraper = WebScraper()
+    WebScraper._add_to_output(webScraper, EXAMPLE_ONE, TXT_FOLDER)
 
-def test_scrape():
-    assert False
+    with open(OUTPUT_FILE) as file:
+        line = file.readlines()[-1].strip()
+
+    assert line == EXAMPLE_ONE
+
+def test_find():
+    if os.path.exists(OUTPUT_FILE):
+        os.remove(OUTPUT_FILE)
+
+    webScraper = WebScraper()
+    WebScraper._find(webScraper, EXAMPLE_ONE, "Home", TXT_FOLDER)
+
+    with open(OUTPUT_FILE) as file:
+        line = len(file.readlines())
+
+    assert line == 6
+
+def test_scrape_create_output():
+    if os.path.exists(OUTPUT_FILE):
+        os.remove(OUTPUT_FILE)
+
+    webScraper = WebScraper()
+    WebScraper.scrape(webScraper, INPUT_FILE, "Laptops", TXT_FOLDER)
+
+    with open(OUTPUT_FILE) as file:
+        line = len(file.readlines())
+
+    assert line == 9
+
+def test_scrape_clear_output():
+    webScraper = WebScraper()
+    WebScraper.scrape(webScraper, INPUT_FILE, "Laptops", TXT_FOLDER)
+
+    with open(OUTPUT_FILE) as file:
+        line = len(file.readlines())
+
+    assert line == 9
--- model/web_scraper.py
+++ model/web_scraper.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 
 class WebScraper:
 
-    def _find_keyword(self, url, keyword):
+    def _find_keyword(self, url: str, keyword: str) -> bool:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         text = soup.get_text()
 
         return keyword in text
 
-    def _find_dirs(self, url):
+    def _find_dirs(self, url: str) -> list[str]:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
 
@@ -18,12 +18,18 @@ class WebScraper:
         for node in soup.find_all('a') 
         if node.get('href').endswith("")}
 
-        return subdirs.add(url)
+        subdirs.add(url)
 
-    def _find(self, url, keyword, output_folder):
+        return subdirs
 
+    def _add_to_output(self, url: str, output_folder: str) -> None :
+        with open(output_folder + "/output.txt", "a") as file:
+            file.write(url + "\n")
+
+    def _find(self, url: str, keyword: str, output_folder: str) -> None:
         # FOR EACH DIRECTORY + SUBDIRECTORY
         dirs = self._find_dirs(url)
+
         for dir in dirs:
 
             # IF KEYWORD SPOTTED
@@ -32,18 +38,24 @@ class WebScraper:
                 # ADD TO THE OUTPUT FILE
                 self._add_to_output(dir, output_folder)
 
-    def _add_to_output(self, url, output_folder):
-        pass
-
-    def scrape(self, input_file, keyword, output_folder):
+    def scrape(self, input_file: str, keyword: str, output_folder: str) -> None:
+        
+        # CREATE OUTPUT FILE OR CLEAR OUTPUT FILE IF IT EXISTS
+        try:
+            with open(output_folder + "/output.txt", "x") as file:
+                pass
+        except:
+            with open(output_folder + "/output.txt", "w") as file:
+                pass
+        
         # FOR EVERY WEBSITE IN THE INPUT TEXT FILE
         with open(input_file) as file:
             lines = file.readlines()
 
             for line in lines:
-
+                print(f"Looking in { line }...")
                 # IF THERE'S A KEYWORD WITHIN THAT WEBSITE
-                self._find(line, keyword, output_folder)
+                self._find(line.strip(), keyword, output_folder)
 
         print("=======================")
         print("IT ALL WORKS!")
